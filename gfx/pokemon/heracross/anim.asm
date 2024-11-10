1b073ab418011cb1d7e45bd37c1b56af86bd75de
tpab19 <brownhtx0817@gmail.com>
2024-11-10 15:20:22 -0600

    update anim
    hera

--- gfx/pokemon/heracross/anim.asm
+++ gfx/pokemon/heracross/anim.asm
@@ -1,10 +1,11 @@
 	frame 0, 06
-	setrepeat 2
+	frame 1, 20
+    frame 2, 18
 	frame 1, 20
 	frame 2, 18
 	frame 1, 20
-	dorepeat 2
 	frame 3, 14
-	frame 4, 14
+	frame 1, 14
 	frame 3, 14
+	frame 1, 14
 	endanim
