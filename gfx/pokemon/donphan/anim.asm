52c02c1070eb5d86fa21e99a56c9c175acb21f7c
tpab19 <brownhtx0817@gmail.com>
2024-11-16 10:21:27 -0600

    temp anim deletions
    donphan, houndoom, victreebell, and hcross

--- gfx/pokemon/donphan/anim.asm
+++ gfx/pokemon/donphan/anim.asm
@@ -1,5 +1,1 @@
-	frame 2, 08
-	frame 1, 24
-	frame 3, 10
-	frame 4, 06
-	endanim
+endanim
--- gfx/pokemon/heracross/anim.asm
+++ gfx/pokemon/heracross/anim.asm
@@ -1,11 +1,1 @@
-	frame 0, 06
-	frame 1, 20
-    frame 2, 18
-	frame 1, 20
-	frame 2, 18
-	frame 1, 20
-	frame 3, 14
-	frame 1, 14
-	frame 3, 14
-	frame 1, 14
-	endanim
+endanim
